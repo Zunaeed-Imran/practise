be927db40b44291296a6a9e97a19ff26c0e88178
imran <zunaeede@gmail.com>
2023-08-19 19:34:34 +0600

    main -make it how untrack. - make it undebug line by line.

--- create.php
+++ create.php
@@ -3,31 +3,13 @@
 require_once('connect.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo "Server request is sucessful<br>";
-
-    $name = uniqid(rand(), true);
-    
-    echo "image also sucessful {$name}<br>";
-    
-    $ext = end((explode(".", $_FILES['image']['name'])));
-    
-    echo "Image extention also sucessful {$ext}<br>";
-    
-    $image = "images/{$name}.{$ext}";
-    
-    echo "Image path also sucessful {$image}<br>";
-    
-
-    $is_moved = move_uploaded_file($_FILES['image']['tmp_name'], $image);
-
-    echo 'Image moved: ' . ($is_moved ? 'Yes' : 'No') . '.<br>';
-    
-    if ($is_moved && $conn->query("INSERT INTO `practice_crud` (`name`, `image`) VALUES ('{$_POST['name']}', '{$image}');"))
-    {
-        echo "upload image also sucessful";    
-        
-        header('location: ./');
-    }
+  $name = uniqid(rand(), true);
+  $ext = end((explode(".", $_FILES['image']['name'])));
+  $image = "images/{$name}.{$ext}";
+  // $image = "images/{$_FILES['image']['name']}";
+  if (move_uploaded_file($_FILES['image']['tmp_name'], $image) && $conn->query("INSERT INTO `practice_crud` (`name`, `image`) VALUES ('{$_POST['name']}', '{$image}');")) {
+    header('location: ./');
+  }
 }
 
 ?>
