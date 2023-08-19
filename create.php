e7acc15a7fae436edf31e5789769ecbc543728e6
imran <zunaeede@gmail.com>
2023-08-19 15:54:53 +0600

    test2 -remove a line the line was comment on create file

--- create.php
+++ create.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $name = uniqid(rand(), true);
   $ext = end((explode(".", $_FILES['image']['name'])));
   $image = "images/{$name}.{$ext}";
-  // $image = "images/{$_FILES['image']['name']}";
+  
   if (move_uploaded_file($_FILES['image']['tmp_name'], $image) && $conn->query("INSERT INTO `practice_crud` (`name`, `image`) VALUES ('{$_POST['name']}', '{$image}');")) {
     header('location: ./');
   }
